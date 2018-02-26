8747bb1c07e01134b12de9c5dc35f6904a9f64bd
Owen Wang <ojwang@stanford.edu>
2018-02-26 12:48:52 -0800

    Made the encoder model have 3 outputs: 56, 28, and 14 res encoding volumes.

--- model/densenet_encoder/construct.py
+++ model/densenet_encoder/construct.py
@@ -19,18 +19,32 @@ print("Image dimensions set to %s." % str(image_dim))
 print("Loading denseNet model...")
 densenet_model = densenet.DenseNetImageNet161(input_shape=image_dim, include_top=False)
 
-
 print("Extracting a subset of denseNet to use as an encoder.")
 n_final_layer = 393
 print("Final layer is %d." % n_final_layer)
+
 inp = densenet_model.input
-out = densenet_model.layers[393].output
+
+res56 = densenet_model.layers[49]
+print("res56 layer is %s" % str(res56))
+out_res56 = res56.output
+
+res28 = densenet_model.layers[137]
+print("res28 layer is %s" % str(res28))
+out_res28 = res28.output
+
+out_layer = densenet_model.layers[393]
+print("out layer is %s" % str(out_layer))
+out = out_layer.output
+
 print("Input shape:  %s" % str(inp))
+print("Res 28 shape: %s" % str(out_res28))
+print("Res 56 shape: %s" % str(out_res56))
 print("Output shape: %s" % str(out))
 
 
-encoder = Model(inputs=inp, outputs=out)
-encoder.summary()
+encoder = Model(inputs=inp, outputs=[out_res56, out_res28, out])
+# encoder.summary()
 
 print("Freezing encoder weights. All layers set trainable=False.")
 for layer in encoder.layers:
