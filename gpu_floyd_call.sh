6d56de101e3e7a88a807756bc78d64314abb9f69
Owen Wang <owenwang0328@gmail.com>
2018-03-05 16:38:17 -0800

    Edited calls to be in quotes for actual command python3... etc. Next step to make an argparse, as soon as the model successfully starts training. Will try GPU now, as CPU ran out of memory.

--- gpu_floyd_call.sh
+++ gpu_floyd_call.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-floyd run --data ojwang/datasets/coco-people/1:/coco --env tensorflow-1.5 --gpu python3 training.py
+floyd run --data ojwang/datasets/coco-people/1:/coco --env tensorflow-1.5 --gpu "python3 training.py"
